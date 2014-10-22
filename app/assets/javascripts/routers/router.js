8c4a9c12f42413f3d331958c6b069f0f6780228e
alexshook <alexandra.shook@gmail.com>
2014-10-21 23:01:45 -0400

    set up router properly

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -2,27 +2,14 @@ var CategoriesRouter = Backbone.Router.extend({
   initialize: function() {
     this.postModel = new PostModel();
     this.categoryModel = new CategoryModel();
+  },
+  routes: {
+    'categories': 'index',
+    'categories/:id': 'showCategory'
+  },
+  index: function() {
+    this.categoryCollection.fetch({reset: true, wait: true});
+  },
+  showCategory: function(id) {
   }
-})
-
-// var PhotoRouter = Backbone.Router.extend({
-//   initialize: function() {
-//     this.streamingView = new StreamingView({ el: $("#index") });
-//     this.photoModel = new PhotoModel();
-//     this.userModel = new UserModel();
-//     this.userCollection = new UserCollection({model: this.userModel});
-//     this.userList = new UserList({collection: this.userCollection});
-//   },
-//   routes: {
-//     'user': 'index',
-//     'users/:id': 'showUser'
-//   },
-//   index: function() {
-//     this.userCollection.fetch({reset: true, wait: true});
-//   },
-//   showUser: function(id) {
-//     this.userCollection.fetch({success: function() {
-//       this.userList.filter(id);
-//     }.bind(this)})
-//   }
-// });
+});
