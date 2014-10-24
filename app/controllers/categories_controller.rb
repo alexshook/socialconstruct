d104b16395f4dee0a49c1dcc522c3d3f819037a3
alexshook <alexandra.shook@gmail.com>
2014-10-24 18:48:37 -0400

    switch root from blogs to categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,6 @@
+class CategoriesController < ApplicationController
+  def index
+    # @current_category = Category.find_by_name "nyfw"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'blogs#index'
+  root to: 'categories#index'
 
   get 'signup', to: 'members#new'
   # post 'signup', to: 'member#create'
