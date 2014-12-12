20a5093b84670f3b819d6ea375e5fd5f31f299f5
alexshook <alexandra.shook@gmail.com>
2014-12-11 23:25:40 -0500

    add authorization check for admins only dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,13 @@
 class Admin::DashboardController < ApplicationController
-  include ApplicationHelper
+  before_action :check_role, only: [ :index ]
+
   def index
     @member = Member.new
+  end
+
+  private
+  def check_role
+    redirect_to index_path unless current_member.admin?
   end
 
 end
