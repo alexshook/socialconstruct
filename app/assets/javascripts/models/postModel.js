2213d79d6469d60f0c7f8512aa4c7c7fdb20549a
alexshook <alexandra.shook@gmail.com>
2014-12-11 23:27:41 -0500

    begin thinking about front end parsing for post syndication

--- app/assets/javascripts/models/postModel.js
+++ app/assets/javascripts/models/postModel.js
@@ -4,6 +4,14 @@ var PostModel = Backbone.Model.extend({
     img_url: 'http://placekitten.com/300/300'
   },
   initialize: function() {
-    console.log("hi i'm the photo model");
+    console.log("hi i'm the post model");
+  },
+  fetchLatestWordpress: function() {
+    // $.get '/fetch'
+    // _.each
+  },
+  fetchLatestBlogger: function() {
+
   }
+
 });
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,10 @@
+require 'open-uri'
+
 class PostsController < ApplicationController
+  # will need to separate wordpress, blogger, and parse differently on front end as well
+  def create
+    feed = open('http://delayedmissives.com/feed').read
+    render text: feed
+  end
+
 end
