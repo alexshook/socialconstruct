05b68926df1e3473252b79b9a93a545ee18c354a
alexshook <alexandra.shook@gmail.com>
2014-12-23 20:14:45 -0500

    removing has_secure_password validations bypass for role :blogger

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,10 +3,7 @@ class Member < ActiveRecord::Base
 
   has_many :blogs
 
-  has_secure_password validations: false
-  # if role is blogger, bypass password
-  validates_presence_of :password, on: :create, unless: :blogger?
-  validates_confirmation_of :password, if: :password
+  has_secure_password
 
   validates :email, uniqueness: true
 end
